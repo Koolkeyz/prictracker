c15fa0f464789cbced039018571674e5a6a02f1a
Adrian Gookool <amgookool@hotmail.com>
2025-06-01 17:45:05 -0400

    ✨ Added Form Message component and Iconify Icon

---
+++ frontend/src/components/ui/FormMessage.svelte
@@ -0,0 +1,31 @@
+<script lang="ts" module>
+	export interface FormMessageType {
+		type: 'error' | 'success' | 'info';
+		message: string;
+	}
+</script>
+
+<script lang="ts">
+	import { type HTMLAttributes } from 'svelte/elements';
+	import Icon from './Icon.svelte';
+
+	interface FormMessageProps extends HTMLAttributes<HTMLDivElement> {
+		type?: 'error' | 'success' | 'info';
+		message: string;
+	}
+
+	const { type, message, ...props }: FormMessageProps = $props();
+
+	const iconSize = 'size-5'; // Default icon size, can be adjusted as needed
+</script>
+
+<div {...props} class={['flex items-center justify-start gap-1.5', props.class]}>
+	{#if type === 'error'}
+		<Icon name="mdi--close-circle-outline" class={['text-error', iconSize]} />
+	{:else if type === 'success'}
+		<Icon name="mdi--tick-circle-outline" class={['text-success', iconSize]} />
+	{:else if type === 'info'}
+		<Icon name="mdi--information-variant-circle-outline" class={['text-warning', iconSize]} />
+	{/if}
+	<span class="text-sm">{message}</span>
+</div>
